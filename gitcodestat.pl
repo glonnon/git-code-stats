0d44d5b5b1e6c592cac40139376a3d18cba07c59
Greg Lonnon <greg.lonnon@gmail.com>
2010-10-01 07:38:13 -0600

    added gitcodestat stuff

--- gitcodestat.pl
+++ gitcodestat.pl
@@ -1,51 +1,90 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+use strict;
 
 
 sub ProcessPatchFile
 {
+    my @patch = @_;
+    my $line;
+    print "Process Patch File\n";
 
-
-}
-
-ProcessPatchFile
-{
-    my @lines = $_;
+    # process the header
+    my $header = 1;
+    my $index = 0;
+    my @curfile;
 
     my $modified = 0;
     my $added = 0;
     my $deleted = 0;
     my $chunkadd = 0;
     my $chunkdel = 0;
+    my $chunk;
+    my %curfile;
+    my $tmp;
 
-    foreach $line (@lines)
+    foreach $line (@patch)
     {
-        #parse the patch file (first character is key)
-        if(/^--- (.*)/)
+        chomp($line);
+
+        if($line =~ /^diff --git (.*) (.*)/)
         {
-            $orginal=$1;
+            print "here **\n";
+            if(keys(%curfile) != 0)
+            {
+                print "here ***\n";
+                $curfile{'modified'} = $modified;
+                $curfile{"added"}    = $added;
+                $curfile{"deleted"}  = $deleted;
+                push(@patch,%curfile);
+            }
+            %curfile = ();
         }
-        elsif (/^+++ (.*)/)
+        elsif($line =~ /^index .*/)
         {
-            $new=$1;
+
         }
-        elsif(/^@@ -(d+),(d+) +(d+),(d+)/)
+        elsif($line =~/^similarity index (.*)/)
         {
-            $patchchanges = abs($2 - $4);
-            $patch = 1;
+            $curfile{"similarity"} = $1;
         }
-        elsif(/^ /)
+        elsif($line =~/^(rename|copy) from (.*)/)
+        {
+            $curfile{"mode"} = $1;
+            $curfile{"from"} = $2;
+        }
+        elsif($line =~/^(rename|copy) to (.*)/)
+        {
+            $curfile{"to"} = $2;
+        }
+        elsif($line =~/^new file mode .*/)
+        {
+            $curfile{"mode"} = "new";
+        }
+        elsif(/^--- (.*)/)
+        {
+            $curfile{"orignal"} = $1;
+        }
+        elsif (/^\+\+\+ (.*)/)
+        {
+            $curfile{"new"} = $1;
+        }
+        elsif(/^@@ -(d+),(d+) \+(d+),(d+) @@/)
+        {
+
+        }
+        elsif(/^ .* / && $chunk != 0)
         {
             # figure out the added, deleted, modified
             if($chunkadd != 0 && $chunkdel != 0)
             {
                 if($chunkadd > $chunkdel)
                 {
-                    $modify += $chunkdel;
+                    $modified += $chunkdel;
                     $added += $chunkadd - $chunkdel;
                 }
                 else
                 {
-                    $modify += $chunkadd;
+                    $modified += $chunkadd;
                     $deleted += $chunkdel - $chunkadd;
                 }
             }
@@ -56,36 +95,50 @@ ProcessPatchFile
             }
             $chunkadd = 0;
             $chunkdel = 0;
+            $chunk = 0;
         }
-        elsif(/^-/)
+        elsif(/^[-](.*)/)
         {
             $chunkdel++;
-
+            $chunk = 1;
         }
-        elsif(/^+/)
+        elsif(/^[+](.*)/)
         {
+            print "line3 :$line\n";
             $chunkadd++;
+            $chunk = 1;
+        }
+        else
+        {
+            print "line2 :$line\n";
         }
     }
-    # catch the last chunck
-    return ($orig,$new,$added,$deleted,$modified);
+    return %curfile;
 }
-
 
 # input is a list of hashes
 
-my $rev1 = <STDIN>;
+open(TEST,"git rev-list --reverse HEAD |");
+
+my $rev1 = <TEST>;
+chomp($rev1);
 my $rev2;
 
-while(<STDIN>)
+while(<TEST>)
 {
-    $rev2 = <STDIN>;
-    open(<PATCH>,"git diff -p $rev1..$rev2 |");
-    my $patch = <PATCH>;
-    close(<PATCH>);
-    
-    ProcessPatchFile(
-    
-
-
+    $rev2 = $_;
+    chomp($rev2);
+    print "looking at COMMIT: $rev1..$rev2\n";
+    open(PATCH,"git diff -M $rev1..$rev2 |") or die "failed to gif diff";
+    my @patch = <PATCH>;
+    close(PATCH);
+    my %file;
+    %file = ProcessPatchFile(@patch);
+    my $key;
+    my $value;
+    while(($key,$value) = each (%file))
+    {
+        print "$key : $value\n";
+    }
+    $rev1 = $rev2;
 }
